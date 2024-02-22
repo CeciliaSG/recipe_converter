b20da14892fe9643b1c16e0f6f73a8a9f985d670
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-02-22 18:47:47 +0000

    Fix unconverted_measurements.append

--- run.py
+++ run.py
@@ -6,27 +6,28 @@ from collections import ChainMap
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('recipe_converter')
+SHEET = GSPREAD_CLIENT.open("recipe_converter")
 
 
 def get_user_recipe_choice():
     """
     Gets the users's recipe choice.
     """
-    print('Please choose a recipe from our recipe bank')
-    print('Recipes to choose from:\n')
-    pprint(', '.join(worksheet_titles).title())
+    print("Please choose a recipe from our recipe bank")
+    print("Recipes to choose from:\n")
+    pprint(", ".join(worksheet_titles).title())
 
-    user_choice = input('Please enter your choice: \n').lower()
-    print(f'You chose {user_choice}\n')
+    user_choice = input("Please enter your choice: \n").lower()
+    print(f"You chose {user_choice}\n")
 
-    return user_choice 
+    return user_choice
+
 
 def validate_user_recipe_choice(user_choice, worksheet_titles):
     """
@@ -34,107 +35,111 @@ def validate_user_recipe_choice(user_choice, worksheet_titles):
     """
     while True:
         if user_choice in worksheet_titles:
-              print(f"You have chosen {user_choice}. This recipe is available.")
-              break
+            print(f"You have chosen {user_choice}. This recipe is available.")
+            break
         else:
-              print(f"Your choice: {user_choice}. No such recipe. Please choose recipe in our recipe bank.") 
-              user_choice = input('Please enter your choice: ').lower()
-    
-    return user_choice          
+            print(
+                f"Your choice: {user_choice}. No such recipe. Please choose recipe in our recipe bank."
+            )
+            user_choice = input("Please enter your choice: ").lower()
+
+    return user_choice
+
 
 worksheet_titles = [worksheet.title.lower() for worksheet in SHEET.worksheets()]
-       
-  
+
+
 def get_required_portions():
     """
-    Ask the user to input the required number of portions 
+    Ask the user to input the required number of portions
     for the recipe to use in calculation for ingredients amounts
     """
     while True:
-        print('Enter number of portions for recipe: ')
+        print("Enter number of portions for recipe: ")
 
         try:
-            user_portions = int(input('Please enter number of portions: \n'))
-            print(f'Portions: {user_portions}\n')
+            user_portions = int(input("Please enter number of portions: \n"))
+            print(f"Portions: {user_portions}\n")
 
             if validate_user_portions(user_portions):
                 print("Portions ✔")
                 break
 
             else:
-                print('Not a valid choice. Please try again')
+                print("Not a valid choice. Please try again")
 
-        except ValueError:  
-            print('Invalid input. Please enter a number.') 
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
     return user_portions
 
+
 def validate_user_portions(value):
     """
-    Validates the user_input for number of portions, 
+    Validates the user_input for number of portions,
     only one number allowed
     """
     try:
         value = int(value)
         if value < 1 or value > 100:
-            raise ValueError(
-                f"Choose a number between 1 and 100, you provided {value}"
-            )
-    except ValueError as e:   
-        print(f"Not a correct choice: {e}")  
+            raise ValueError(f"Choose a number between 1 and 100, you provided {value}")
+    except ValueError as e:
+        print(f"Not a correct choice: {e}")
         return False
 
     return True
 
-def input_request_metric_imperial(new_recipe, new_recipe_imperial):   
 
+def input_request_metric_imperial(new_recipe, new_recipe_imperial):
     """
-    Lets the user chose if they want the measurements in imperial 
+    Lets the user chose if they want the measurements in imperial
     or metric units, requires user to input correct answer
-    """     
-    while True:
-        unit_choice = input('Please choose imperial/metric: \n')
-        if unit_choice.lower() == 'metric':
-                display_recipe_metric(new_recipe)
-                break
-
-        elif unit_choice.lower() == 'imperial':
-                display_recipe_imperial(new_recipe_imperial)
-                break
-
-        else: 
-                print('Please enter valid choice')
-
-    return unit_choice        
-
-  
-def get_user_choice_ingredients(user_choice):
-
     """
-    Access the ingredients for the user's chosen recipe 
+    while True:
+        unit_choice = input("Please choose imperial/metric: \n")
+        if unit_choice.lower() == "metric":
+            display_recipe_metric(new_recipe)
+            break
+
+        elif unit_choice.lower() == "imperial":
+            display_recipe_imperial(new_recipe_imperial)
+            break
+
+        else:
+            print("Please enter valid choice")
+
+    return unit_choice
+
+
+def get_user_choice_ingredients(user_choice):
+    """
+    Access the ingredients for the user's chosen recipe
     and returns the int or float values for calculation
     """
     ingredients = SHEET.worksheet(user_choice).get_all_values()
 
     ingredients_column = [row[2] for row in ingredients[1:]]
-  
+
     return ingredients_column
 
 
 def calculate_user_measurements(ingredients_column, user_portions):
     """
-    Calculates the measurements with the users 
+    Calculates the measurements with the users
     chosen portions and recipe ingredients
     """
 
     ingredients_float = [float(ingredient) for ingredient in ingredients_column]
-    new_measurements =  [round(ingredient * user_portions, 1) for ingredient in ingredients_float]
+    new_measurements = [
+        round(ingredient * user_portions, 1) for ingredient in ingredients_float
+    ]
 
     return new_measurements
 
+
 def display_recipe_new_measurements(user_choice, new_measurements):
     """
-    Create the new recipe with the user's requested measurements, 
+    Create the new recipe with the user's requested measurements,
     and add the measurement labels and ingredient headings
     """
     data = SHEET.worksheet(user_choice).get_all_values()
@@ -142,14 +147,19 @@ def display_recipe_new_measurements(user_choice, new_measurements):
 
     metric_measurements = [row[1] for row in data[1:]]
 
-    new_recipe = {heading: f"{measurement} {metric_measurements}" for heading, measurement, metric_measurements in zip(headings_column, new_measurements, metric_measurements)}  
+    new_recipe = {
+        heading: f"{measurement} {metric_measurements}"
+        for heading, measurement, metric_measurements in zip(
+            headings_column, new_measurements, metric_measurements
+        )
+    }
     return new_recipe, metric_measurements
 
 
 def convert_metrics_to_imperial_units(new_recipe, user_choice):
     """
-    Convert the new_recipe metric units to imperial 
-    units. If there is litres, dl, grams or kg in the recipe, 
+    Convert the new_recipe metric units to imperial
+    units. If there is litres, dl, grams or kg in the recipe,
     convert to gallons, ounces, cups and pounds.
 
     1kg = 2.2046 pounds
@@ -165,106 +175,124 @@ def convert_metrics_to_imperial_units(new_recipe, user_choice):
         conversion = False
 
         try:
-            if 'gram' in measurement:
+            if "gram" in measurement:
                 quantity = measurement.split()[0]
                 converted_measurement_ounces = round(float(quantity) * 0.03527, 1)
                 converted_measurements.append(converted_measurement_ounces)
                 conversion = True
                 print("Converted_to_ounces:", converted_measurement_ounces, "ounces")
         except KeyError:
-            pass        
+            pass
 
         try:
-            if 'dl' in measurement:
+            if "dl" in measurement:
                 quantity = measurement.split()[0]
                 converted_measurement_dl_cups = round(float(quantity) * 0.422675284, 1)
                 converted_measurements.append(converted_measurement_dl_cups)
                 conversion = True
                 print(converted_measurement_dl_cups, "cups")
         except KeyError:
-            pass        
-            
-        try:    
-            if 'kg' in measurement:
+            pass
+
+        try:
+            if "kg" in measurement:
                 quantity = measurement.split()[0]
-                converted_measurement_pounds = round(float(quantity) * 2.2046, 1) 
+                converted_measurement_pounds = round(float(quantity) * 2.2046, 1)
                 converted_measurements.append(converted_measurement_pounds)
                 conversion = True
                 print(converted_measurement_pounds, "pounds")
         except KeyError:
-            pass        
+            pass
 
-        try:     
-            if 'litres' in measurement:
+        try:
+            if "litres" in measurement:
                 quantity = measurement.split()[0]
-                converted_measurement_litres_cups = round(float(quantity) * 4.22675284, 1)
+                converted_measurement_litres_cups = round(
+                    float(quantity) * 4.22675284, 1
+                )
                 converted_measurements.append(converted_measurement_litres_cups)
                 conversion = True
-                print(converted_measurement_litres_cups, "cups")    
+                print(converted_measurement_litres_cups, "cups")
         except KeyError:
-            pass     
+            pass
 
         if not conversion:
-            unconverted_measurements.append((heading, new_recipe)) 
-  
+            unconverted_measurements.append([heading, measurement])
 
+    print("unconverted_measurements1:", unconverted_measurements)
     data = SHEET.worksheet(user_choice).get_all_values()
     headings_column = [row[0] for row in data[1:]]
     imperial_measurements = [row[3] for row in data[1:]]
 
-    new_recipe_imperial = {heading: f"{measurement} {converted_measurement}" for heading, measurement, converted_measurement in zip(headings_column, converted_measurements, imperial_measurements)}     
-    unconverted_measurements = {heading: measurement for heading, measurement in zip(headings_column, unconverted_measurements)}     
+    new_recipe_imperial = {
+        heading: f"{measurement} {converted_measurement}"
+        for heading, measurement, converted_measurement in zip(
+            headings_column, converted_measurements, imperial_measurements
+        )
+    }
+    # print('new_recipe_imperial:', new_recipe_imperial)
+    # unconverted_measurements_dict= { heading: measurement
+    # for heading, measurement in zip(headings_column, unconverted_measurements)
+    # }
+    # print('unconverted_measurements:', unconverted_measurements)
 
-    new_recipe_imperial = ChainMap(new_recipe_imperial, unconverted_measurements) 
-    print('New:', new_recipe_imperial)
+    newer_recipe_imperial = ChainMap(new_recipe_imperial, unconverted_measurements)
+    print("New:", newer_recipe_imperial)
 
-    print('unconverted:', unconverted_measurements)
-    #print('imperial:', new_recipe_imperial)
-    return new_recipe_imperial
+    return newer_recipe_imperial
+
 
 def display_recipe_metric(new_recipe):
     print(new_recipe)
 
-def display_recipe_imperial(new_recipe_imperial):
-    s = pformat(new_recipe_imperial)
-    print('Recipe_imperial:', s)
-    #print(new_recipe_imperial)
+
+def display_recipe_imperial(newer_recipe_imperial):
+    s = pformat(newer_recipe_imperial)
+    print("Recipe_imperial:", s)
+
 
 def main():
-
     """
     Run all program functions
     """
 
     while True:
-    
+
         user_choice = get_user_recipe_choice()
         user_choice = validate_user_recipe_choice(user_choice, worksheet_titles)
         user_portions = get_required_portions()
         ingredients_column = get_user_choice_ingredients(user_choice)
-        new_measurements = calculate_user_measurements(ingredients_column, user_portions)
-        new_recipe, metric_measurements = display_recipe_new_measurements(user_choice, new_measurements)
+        new_measurements = calculate_user_measurements(
+            ingredients_column, user_portions
+        )
+        new_recipe, metric_measurements = display_recipe_new_measurements(
+            user_choice, new_measurements
+        )
         new_recipe_imperial = convert_metrics_to_imperial_units(new_recipe, user_choice)
         unit_choice = input_request_metric_imperial(new_recipe, new_recipe_imperial)
 
         while True:
-        
-            user_choice_two = input('Do you want to cook something else? (yes/no) \n').lower()
-            
-            if user_choice_two == 'no':
+
+            user_choice_two = input(
+                "Do you want to cook something else? (yes/no) \n"
+            ).lower()
+
+            if user_choice_two == "no":
                 break
 
-            elif user_choice_two == 'yes':
+            elif user_choice_two == "yes":
                 break
 
             else:
-                print('Invalid choice. Please enter yes or no')
+                print("Invalid choice. Please enter yes or no")
 
-        if user_choice_two == 'no':
+        if user_choice_two == "no":
             break
 
-    print('Thank you for using our recipe converter.')
-    
+    print("Thank you for using our recipe converter.")
 
-print('Welcome to our recipe bank, where you can convert each recipe \n for the exact number of portions you are cooking')
+
+print(
+    "Welcome to our recipe bank, where you can convert each recipe \n for the exact number of portions you are cooking"
+)
 main()
