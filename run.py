a04cb3d43f3c6673cae1922d95a102e0a59cf2c1
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-02-25 20:18:53 +0000

    Round calculation for ounces to lbs

--- run.py
+++ run.py
@@ -77,8 +77,7 @@ def validate_user_portions(value):
     try:
         value = int(value)
         if value < 1 or value > 100:
-            raise ValueError(
-                    f"Choose a number between 1 and 100, you provided {value}")
+            raise ValueError(f"Choose a number between 1 and 100, you provided {value}")
     except ValueError as e:
         print(f"Not a correct choice: {e}")
         return False
@@ -125,11 +124,9 @@ def calculate_user_measurements(ingredients_column, user_portions):
     chosen portions and recipe ingredients
     """
 
-    ingredients_float = [float(ingredient)
-                         for ingredient in ingredients_column]
+    ingredients_float = [float(ingredient) for ingredient in ingredients_column]
     new_measurements = [
-        round(ingredient * user_portions, 1)
-        for ingredient in ingredients_float
+        round(ingredient * user_portions, 1) for ingredient in ingredients_float
     ]
 
     return new_measurements
@@ -151,7 +148,7 @@ def display_recipe_new_measurements(user_choice, new_measurements):
             headings_column, new_measurements, metric_measurements_column
         )
     }
-    print('new_reipe:', new_recipe)
+    print("new_reipe:", new_recipe)
     return new_recipe, metric_measurements_column
 
 
@@ -167,12 +164,13 @@ def convert_large_metrics_to_new_units(new_recipe):
         if unit == "dl" and quantity >= 10:
             new_recipe[ingredient] = f"{quantity / 10} litre(s)"
 
-        #if unit == "tsp" and quantity >= 3:
-            #new_recipe[ingredient] = f"{quantity / 3} tbsp"  
+        # if unit == "tsp" and quantity >= 3:
+        # new_recipe[ingredient] = f"{quantity / 3} tbsp"
 
-        #if unit == "tbsp" and quantity >= 6.7:
-            #new_recipe[ingredient] = f"{quantity / 6.8} dl"      
-    print('new_recipe:', new_recipe)
+        # if unit == "tbsp" and quantity >= 6.7:
+        # new_recipe[ingredient] = f"{quantity / 6.8} dl"
+        #
+    print("new_recipe:", new_recipe)
     return new_recipe
 
 
@@ -202,27 +200,24 @@ def convert_metrics_to_imperial_units(new_recipe, user_choice):
 
         if "dl" in measurement:
             quantity = measurement.split()[0]
-            converted_measurement_dl_cups = round(
-                    float(quantity) * 0.422675284, 1)
+            converted_measurement_dl_cups = round(float(quantity) * 0.422675284, 1)
             converted_measurements.append(converted_measurement_dl_cups)
             conversion = True
 
         if "kg" in measurement:
             quantity = measurement.split()[0]
-            converted_measurement_pounds = round(
-                    float(quantity) * 2.2046, 1)
+            converted_measurement_pounds = round(float(quantity) * 2.2046, 1)
             converted_measurements.append(converted_measurement_pounds)
             conversion = True
 
         if "litre(s)" in measurement:
             quantity = measurement.split()[0]
-            converted_measurement_litres_cups = round(
-                    float(quantity) * 4.22675284, 1)
+            converted_measurement_litres_cups = round(float(quantity) * 4.22675284, 1)
             converted_measurements.append(converted_measurement_litres_cups)
             conversion = True
 
         if not conversion:
-            unconverted_measurements.append((heading, measurement))   
+            unconverted_measurements.append((heading, measurement))
 
     data = SHEET.worksheet(user_choice).get_all_values()
     headings_column = [row[0] for row in data[1:]]
@@ -234,8 +229,23 @@ def convert_metrics_to_imperial_units(new_recipe, user_choice):
             headings_column, converted_measurements, imperial_measurements
         )
     ]
-
+    print("new_recipe_imperial:", new_recipe_imperial)
+    print("newer_recipe_imperial:", new_recipe_imperial)
     newer_recipe_imperial = new_recipe_imperial + unconverted_measurements
+
+    return new_recipe_imperial, newer_recipe_imperial
+
+
+def convert_large_imperial_to_new_units(newer_recipe_imperial):
+
+    for i in range(len(newer_recipe_imperial)):
+        ingredient, measurement = newer_recipe_imperial[i]
+        quantity, unit = measurement.split()
+        quantity = float(quantity)
+
+        if unit == "ounces" and quantity >= 16:
+            measurement = round(quantity / 16, 1)
+            newer_recipe_imperial[i] = ingredient, f"{measurement}pounds"
 
     return newer_recipe_imperial
 
@@ -263,9 +273,7 @@ def custom_print(obj):
         return "\n".join(f"{item[0]}: {item[1]}" for item in obj) + "\n"
 
     if isinstance(obj, dict):
-        return "\n".join(
-            f"{key}: {value}"
-            for key, value in obj.items()) + "\n"
+        return "\n".join(f"{key}: {value}" for key, value in obj.items()) + "\n"
 
 
 def main():
@@ -273,13 +281,11 @@ def main():
     Run all program functions
     """
 
-    worksheet_titles = [worksheet.title.lower()
-                        for worksheet in SHEET.worksheets()]
+    worksheet_titles = [worksheet.title.lower() for worksheet in SHEET.worksheets()]
 
     while True:
         user_choice = get_user_recipe_choice(worksheet_titles)
-        user_choice = validate_user_recipe_choice(
-                user_choice, worksheet_titles)
+        user_choice = validate_user_recipe_choice(user_choice, worksheet_titles)
         user_portions = get_required_portions()
         ingredients_column = get_user_choice_ingredients(user_choice)
         new_measurements = calculate_user_measurements(
@@ -290,10 +296,12 @@ def main():
         )
         new_recipe = convert_large_metrics_to_new_units(new_recipe)
 
-        new_recipe_imperial = convert_metrics_to_imperial_units(
-                new_recipe, user_choice)
-        unit_choice = input_request_metric_imperial(
-                new_recipe, new_recipe_imperial)
+        newer_recipe_imperial, new_recipe_imperial = convert_metrics_to_imperial_units(
+            new_recipe, user_choice
+        )
+
+        convert_large_imperial_to_new_units(new_recipe_imperial)
+        unit_choice = input_request_metric_imperial(new_recipe, new_recipe_imperial)
 
         while True:
 
