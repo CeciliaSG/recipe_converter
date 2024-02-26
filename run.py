9e77465b07ba4321f9a5bea6c391f32517f353ed
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-02-26 20:32:04 +0000

    Remove white spaces, fix indentation and long lines

--- run.py
+++ run.py
@@ -179,10 +179,9 @@ def convert_large_metrics_to_new_units(new_recipe):
             quantity_rounded = round(quantity / 3, 1)
             new_recipe[ingredient] = f"{quantity_rounded} tbsp"
 
-    print('Metric_large:', new_recipe)        
+    print('Metric_large:', new_recipe)
 
     return new_recipe
-
 
 
 def convert_metrics_to_imperial_units(new_recipe, user_choice):
@@ -196,78 +195,70 @@ def convert_metrics_to_imperial_units(new_recipe, user_choice):
     1litre = 4.22675284 cups
     1dl = 0.422675284 cups
     """
-  
+
     converted_measurements = []
     unconverted_measurements = []
-     
+
     conversion_mappings = [
-    ('gram', 'ounces'),
-    ('dl', 'cups'),
-    ('kg', 'pounds'),
-    ('litre(s)', 'cups')
-]
-    
+        ('gram', 'ounces'),
+        ('dl', 'cups'),
+        ('kg', 'pounds'),
+        ('litre(s)', 'cups')
+    ]
+
     for heading, measurement in new_recipe.items():
         conversion = False
-        
+
         for metric_unit, imperial_unit in conversion_mappings:
-        
+
             if metric_unit in measurement:
                 quantity = measurement.split()[0]
                 if metric_unit == "gram":
                     converted_measurement_ounces = round(
-                    float(quantity) * 0.03527, 1)
+                        float(quantity) * 0.03527, 1)
                     converted_measurements.append(
-                    (converted_measurement_ounces, 'ounces'))
+                        (converted_measurement_ounces, 'ounces'))
                 conversion = True
-                
 
             if "dl" in measurement:
                 quantity = measurement.split()[0]
                 converted_measurement_dl_cups = round(
-                    float(quantity) * 0.422675284, 1)
+                        float(quantity) * 0.422675284, 1)
                 converted_measurements.append(
                     (converted_measurement_dl_cups, 'cups'))
                 conversion = True
-                
 
             if "kg" in measurement:
                 quantity = measurement.split()[0]
                 converted_measurement_pounds = round(
-                    float(quantity) * 2.2046, 1)
+                        float(quantity) * 2.2046, 1)
                 converted_measurements.append(
                     (converted_measurement_pounds, 'pounds'))
-                conversion = True 
-                
+                conversion = True
 
             if "litre(s)" in measurement:
                 quantity = measurement.split()[0]
                 converted_measurement_litres_cups = round(
-                    float(quantity) * 4.22675284, 1)
+                        float(quantity) * 4.22675284, 1)
                 converted_measurements.append(
                     (converted_measurement_litres_cups, 'cups'))
                 conversion = True
-                
 
         if not conversion:
-                unconverted_measurements.append((heading, measurement))       
+            unconverted_measurements.append((heading, measurement))
 
     data = SHEET.worksheet(user_choice).get_all_values()
     headings_column = [row[0] for row in data[1:]]
-    #imperial_measurements = [row[3] for row in data[1:]]
-
-    #print('unconverted:', unconverted_measurements)
 
     new_recipe_imperial = [
         (heading, f"{converted_measurement} {unit}")
-        for heading, (converted_measurement, unit) in zip(headings_column, converted_measurements)
+        for heading, (converted_measurement, unit) in zip(
+            headings_column, converted_measurements)
     ]
 
     newer_recipe_imperial = new_recipe_imperial + unconverted_measurements
     print('newer:', newer_recipe_imperial)
-    print('new:', new_recipe_imperial)
     return new_recipe_imperial, newer_recipe_imperial
-  
 
 
 def display_recipe_metric(new_recipe):
@@ -297,6 +288,7 @@ def custom_print(obj):
             f"{key}: {value}"
             for key, value in obj.items()) + "\n"
 
+
 def main():
     """
     Run all program functions
@@ -317,15 +309,15 @@ def main():
         new_recipe, metric_measurements_column = \
             display_recipe_new_measurements(
                 user_choice, new_measurements)
-        
+
         new_recipe = convert_large_metrics_to_new_units(new_recipe)
-        
+
         newer_recipe_imperial, new_recipe_imperial = \
             convert_metrics_to_imperial_units(
                 new_recipe, user_choice)
 
-        new_recipe = convert_tbsp_to_dl(new_recipe)  
-        
+        new_recipe = convert_tbsp_to_dl(new_recipe)
+
         unit_choice = input_request_metric_imperial(
             new_recipe, new_recipe_imperial)
 
